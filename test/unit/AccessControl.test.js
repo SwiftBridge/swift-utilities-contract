fe3200401a43eea030e82006fe37f0967f9a7a02
winsznx <timjosh507@gmail.com>
2025-12-17 20:53:52 +0100

    test: add AccessControl.test.js

---
+++ test/unit/AccessControl.test.js
@@ -0,0 +1,9 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("AccessControl", function () {
+    // tested via UtilitiesV2
+    it("Should act as a placeholder for specific AccessControl tests", function () {
+        expect(true).to.be.true;
+    });
+});

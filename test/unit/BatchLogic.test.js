942e5c4c1121327c8b767b314e983e9dccb6ccdb
winsznx <timjosh507@gmail.com>
2025-12-17 20:53:52 +0100

    test: add BatchLogic.test.js

---
+++ test/unit/BatchLogic.test.js
@@ -0,0 +1,9 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("BatchLogic", function () {
+    // tested via UtilitiesV2
+    it("Should act as a placeholder for specific BatchLogic tests", function () {
+        expect(true).to.be.true;
+    });
+});
